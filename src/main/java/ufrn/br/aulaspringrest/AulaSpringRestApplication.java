f9d2fe80101477119de831ffd4435760cc510bf0
tanir <tanirocr@gmail.com>
2023-08-14 15:56:50 -0300

    Initial commit

---
+++ src/main/java/ufrn/br/aulaspringrest/AulaSpringRestApplication.java
@@ -0,0 +1,13 @@
+package ufrn.br.aulaspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufrn/br/aulaspringrest/AulaSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.br.aulaspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
